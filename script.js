20b8e9c8f0f0623d2455b8133a431ef01b6a9aea
Setayesh-sys <mrs.mirzayee@gmail.com>
2024-07-30 20:48:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,332 @@
+const canvas= document.getElementById("canvas");
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+const context=canvas.getContext("2d");
+
+
+context.fillStyle="red";
+context.fillRect(0,300,502,50);
+
+context.fillStyle="red";
+context.fillRect(1012,300,500,50);
+
+context.fillStyle="red";
+context.fillRect(480,70,550,50);
+
+context.fillStyle="orange";
+context.fillRect(0,350,502,500);
+
+context.fillStyle="orange";
+context.fillRect(1012,350,500,500);
+
+context.fillStyle="yellow";
+context.fillRect(502,120,510,800);
+
+context.fillStyle="brown";
+context.fillRect(0,800,2000,70);
+
+
+context.fillStyle="blue";
+context.fillRect(20,400,80,100);
+
+context.fillStyle="blue";
+context.fillRect(200,400,80,100);
+
+context.fillStyle="blue";
+context.fillRect(370,400,80,100);
+
+context.fillStyle="blue";
+context.fillRect(20,600,80,100);
+
+context.fillStyle="blue";
+context.fillRect(200,600,80,100);
+
+context.fillStyle="blue";
+context.fillRect(370,600,80,100);
+
+context.fillStyle="blue";
+context.fillRect(1050,400,80,100);
+
+context.fillStyle="blue";
+context.fillRect(1210,400,80,100);
+
+context.fillStyle="blue";
+context.fillRect(1370,400,80,100);
+
+context.fillStyle="blue";
+context.fillRect(1050,600,80,100);
+
+context.fillStyle="blue";
+context.fillRect(1210,600,80,100,);
+
+context.fillStyle="blue";
+context.fillRect(1370,600,80,100);
+
+context.fillStyle="yellow";
+context.fillRect(20,500,80,20);
+
+context.fillStyle="yellow";
+context.fillRect(200,500,80,20);
+
+context.fillStyle="yellow";
+context.fillRect(370,500,80,20);
+
+context.fillStyle="yellow";
+context.fillRect(20,700,80,20);
+
+context.fillStyle="yellow";
+context.fillRect(200,700,80,20);
+
+context.fillStyle="yellow";
+context.fillRect(370,700,80,20);
+
+
+context.fillStyle="yellow";
+context.fillRect(1050,500,80,20);
+
+context.fillStyle="yellow";
+context.fillRect(1210,500,80,20);
+
+context.fillStyle="yellow";
+context.fillRect(1370,500,80,20);
+
+context.fillStyle="yellow";
+context.fillRect(1050,700,80,20);
+
+context.fillStyle="yellow";
+context.fillRect(1210,700,80,20);
+
+context.fillStyle="yellow";
+context.fillRect(1370,700,80,20);
+
+context.fillStyle="blue";
+context.fillRect(560,180,70,100);
+
+context.fillStyle="blue";
+context.fillRect(680,180,70,100);
+
+context.fillStyle="blue";
+context.fillRect(790,180,70,100);
+
+context.fillStyle="blue";
+context.fillRect(890,180,70,100);
+
+context.fillStyle="blue";
+context.fillRect(560,360,70,100);
+
+context.fillStyle="blue";
+context.fillRect(680,360,70,100);
+
+context.fillStyle="blue";
+context.fillRect(790,360,70,100);
+
+context.fillStyle="blue";
+context.fillRect(890,360,70,100);
+
+context.fillStyle="orange";
+context.fillRect(560,280,70,20);
+
+context.fillStyle="orange";
+context.fillRect(680,280,70,20);
+
+context.fillStyle="orange";
+context.fillRect(790,280,70,20);
+
+context.fillStyle="orange";
+context.fillRect(890,280,70,20);
+
+context.fillStyle="orange";
+context.fillRect(560,450,70,20);
+
+context.fillStyle="orange";
+context.fillRect(680,450,70,20);
+
+context.fillStyle="orange";
+context.fillRect(790,450,70,20);
+
+context.fillStyle="orange";
+context.fillRect(890,450,70,20);
+
+context.fillStyle="red";
+context.fillRect(650,580,250,30);
+
+
+
+
+
+
+
+
+// context.fillStyle="yellow";
+// context.fillRect(1300,170,100,100);
+
+// context.fillStyle="brown";
+// context.fillRect(100,500,1300,50); //(distance From left, distance from top, width of the rectangle, height of the rectangle)
+
+// context.fillStyle="blue";
+// context.fillRect(900,22,80,150);
+// //! RECTANGLE
+// context.fillStyle="yellow";
+// context.fillRect(100,50,100,200);
+
+// context.fillStyle="red";
+// context.fillRect(200,60,100,200);
+
+// context.fillStyle="pink";
+// context.fillRect(50,300,200,70);
+
+// context.fillStyle="orange";
+// context.fillRect(300,70,300,600);
+
+// //! Circle
+context.fillStyle="#C80036";
+context.beginPath();
+context.arc(770,550,50,0,Math.PI*2);
+context.fill();
+
+context.fillStyle="white";
+context.beginPath();
+context.arc(770,550,40,0,Math.PI*2);
+context.fill();
+
+context.fillStyle="black";
+context.beginPath();
+context.arc(770,550,7,0,Math.PI*2);
+context.fill();
+
+context.fillStyle="black";
+context.beginPath();
+context.arc(770,520,5,0,Math.PI*2);
+context.fill();
+
+context.fillStyle="black";
+context.beginPath();
+context.arc(770,580,5,0,Math.PI*2);
+context.fill();
+
+context.fillStyle="black";
+context.beginPath();
+context.arc(740,550,5,0,Math.PI*2);
+context.fill();
+
+context.fillStyle="black";
+context.beginPath();
+context.arc(800,550,5,0,Math.PI*2);
+context.fill();
+
+context.fillStyle="black";
+context.fillRect(766,530,10,20);
+
+context.fillStyle="black";
+context.fillRect(766,550,30,8);
+
+context.fillStyle="blue";
+context.fillRect(650,610,250,170);
+
+context.fillStyle="black";
+context.fillRect(650,610,250,10);
+
+context.fillStyle="black";
+context.fillRect(650,610,10,170);
+
+context.fillStyle="black";
+context.fillRect(890,610,10,170);
+
+context.fillStyle="black";
+context.fillRect(650,780,250,10);
+
+context.fillStyle="black";
+context.fillRect(770,610,10,170);
+
+context.fillStyle="black";
+context.fillRect(740,690,10,20);
+
+context.fillStyle="black";
+context.fillRect(800,690,10,20);
+
+context.fillStyle="#758694";
+context.fillRect(650,790,250,15);
+
+context.fillStyle="black";
+context.fillRect(645,800,270,15);
+
+context.fillStyle="#758694";
+context.fillRect(635,810,280,15);
+
+context.fillStyle="black";
+context.fillRect(625,820,290,15);
+
+context.fillStyle="#6EACDA";
+context.beginPath();
+context.arc(400,200,80,0,Math.PI*2);
+context.fill();
+
+context.beginPath();
+context.arc(350,140,70,0,Math.PI*2);
+context.fill();
+
+context.beginPath();
+context.arc(280,190,80,0,Math.PI*2);
+context.fill();
+
+context.fillStyle="#6EACDA";
+context.beginPath();
+context.arc(1380,200,80,0,Math.PI*2);
+context.fill();
+
+context.fillStyle="#6EACDA";
+context.beginPath();
+context.arc(1290,140,70,0,Math.PI*2);
+context.fill();
+
+context.fillStyle="#6EACDA";
+context.beginPath();
+context.arc(1250,190,80,0,Math.PI*2);
+context.fill();
+
+context.fillStyle="#88D66C";
+context.beginPath();
+context.arc(1250,600,100,0,Math.PI*2);
+context.fill();
+
+// (distance From left, distance from top, width of the rectangle, height of the rectangle)
+
+
+context.fillStyle="#914F1E";
+context.fillRect(1250,680,10,900);
+
+context.fillStyle="#88D66C";
+context.beginPath();
+context.arc(150,600,100,0,Math.PI*2);
+context.fill();
+
+context.fillStyle="#914F1E";
+context.fillRect(150,680,10,900);
+// context.fillStyle="green";
+// context.beginPath();
+// context.arc(1000,400,100,0,Math.PI*2);
+// context.fill();
+
+// context.fillStyle="black";
+// context.beginPath();
+// context.arc(500,400,30,0,Math.PI*2);
+// context.fill();
+
+// context.fillStyle="black";
+// context.beginPath();
+// context.arc(1000,400,30,0,Math.PI*2);
+// context.fill();
+// context.fillStyle="purple";
+// context.beginPath();
+// context.arc(450,450,200,0,Math.PI*2);
+// context.fill();
+
+// context.fillStyle="blue";
+// context.beginPath();
+// context.arc(750,750,100,0,Math.PI*2);
+// context.fill();
+
+// context.fillStyle="green";
+// context.beginPath();
+// context.arc(650,700,300,0,Math.PI*2);
+// context.fill();
